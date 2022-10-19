a370ef1e88ac97dc3c482ad757c12d7dc99aef69
divizn <hassanjaved186@gmail.com>
2022-10-19 06:34:56 +0100

    feature: added country next to city name

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -39,7 +39,9 @@ const Weather = () => {
       </div>
       <div className="top-content">
         <h1 className="temp">{data ? data.main.temp + "°" : "-"}</h1>
-        <h1 className="cityName">{data ? data.name : " "}</h1>
+        <h1 className="cityName">
+          {data ? data.name + ", " + data.sys.country : " "}
+        </h1>
         <h1 className="description">
           {data
             ? data.weather[0].main + " (" + data.weather[0].description + ")"
